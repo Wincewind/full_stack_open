1582c30a61330a4833e8ef2a274d6cdb422689db
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-05-13 22:26:45 +0300

    Add exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Header = ({ title }) => {
+  return (
+  <h1>
+    {title}
+  </h1>
+  )
+}
+
 const Button = ( { handleClick, text } ) => {
   return (
     <button onClick={handleClick}>
@@ -22,21 +30,28 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [votes, addVotes] = useState(Array(anecdotes.length).fill(0))
+  const [most_voted_anecdote, setMostVotes] = useState({index: 0, votes: 0})
 
   const handleVoting = (selected) => {
     const updated_votes = [...votes]
-    console.log("votes before",updated_votes)
+    // console.log("votes before",updated_votes)
     updated_votes[selected] += 1
     addVotes(updated_votes)
-    console.log("votes after",updated_votes)
+    // console.log("votes after",updated_votes)
+    if (updated_votes[selected] > most_voted_anecdote.votes)
+    setMostVotes({index: selected, votes: updated_votes[selected]})
   }
 
   return (
     <div>
+      <Header title={"Anecdote of the day"} />
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <Button handleClick={() => handleVoting(selected)} text="vote" />
       <Button handleClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} text="next anecdote" />
+      <Header title={"Anecdote with most votes"} />
+      <p>{anecdotes[most_voted_anecdote.index]}</p>
+      <p>has {most_voted_anecdote.votes} votes</p>
     </div>
   )
 }
