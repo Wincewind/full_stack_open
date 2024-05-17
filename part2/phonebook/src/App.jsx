b832548b5369f4dc7b46001fba3b9b1c617fbbb2
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-05-17 20:38:51 +0300

    Add exercises 2.12.-2.15.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,21 +1,25 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
-const Contacts = ({ persons }) => {
+const Contacts = ({ persons, removePerson }) => {
   return (
     <div>
       {persons.map(person =>
-        <Contact key={person.name} person={person} />
+        <Contact key={person.id} person={person} removePerson={removePerson} />
       )}
     </div>
   )
 }
 
-const Contact = ({ person }) => <div>{person.name} {person.number}</div>
+const Contact = ({ person, removePerson }) => 
+<div>
+  {person.name} {person.number}  <button onClick={() =>
+    removePerson(person.id)}>delete</button>
+</div>
 
 const Filter = ({ handleFilterChange, filterString }) =>
   <form>
-    <input onChange={handleFilterChange} value={filterString} />
+    filter shown with <input onChange={handleFilterChange} value={filterString} />
   </form>
 
 const PersonForm = ({ addContact, handleNameChange, handleNumberChange, newName, newNumber }) =>
@@ -38,28 +42,39 @@ const App = () => {
   const [filterString, setNewFilter] = useState('')
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-      })
+    personService
+    .getAll()
+    .then(initialPersons => {
+      setPersons(initialPersons)
+    })
   }, [])
-  console.log('render', persons.length, 'notes')
 
   const addContact = (event) => {
     event.preventDefault()
     if (!persons.map(person => person.name).includes(newName)) {
       const contactObj = { name: newName, number: newNumber }
-      setPersons(persons.concat(contactObj))
-      setNewName('')
-      setNewNumber('')
-      setNewFilter('')
+      personService
+      .create(contactObj)
+      .then(returnedContact => {
+        setPersons(persons.concat(returnedContact))
+        setNewFilter('')
+      })
     }
     else {
-      alert(`${newName} is already added to phonebook`)
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const person = persons.find(p => p.name === newName)
+        const changedPerson = {...person, number: newNumber} 
+
+        personService
+      .update(changedPerson.id, changedPerson)
+      .then(returnedPerson => {
+        console.log("updated", returnedPerson)
+        setPersons(persons.map(p => p.id !== person.id ? p : returnedPerson))
+      })
+      }
     }
+    setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
@@ -72,6 +87,17 @@ const App = () => {
 
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value)
+  }
+
+  const removePerson = id => {
+    if (window.confirm(`Delete ${persons.find(p => p.id === id).name} ?`)) {
+      personService
+    .remove(id)
+    .then(removedPerson => {
+      console.log("removed", removedPerson)
+      setPersons(persons.filter(p => p.id !== id))
+    })
+    }
   }
 
   return (
@@ -87,7 +113,7 @@ const App = () => {
         newNumber={newNumber}
       />
       <h3>Numbers</h3>
-      <Contacts persons={persons.filter(person => person.name.toLowerCase().includes(filterString.toLowerCase()))} />
+      <Contacts persons={persons.filter(person => person.name.toLowerCase().includes(filterString.toLowerCase()))} removePerson={removePerson} />
     </div>
   )
 
