f195a242bce649deb91d90e7e53863ac7bb61f30
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-08-20 16:03:33 +0300

    Add exercise 4.4

--- part4/bloglist/backend/tests/list_helper.test.js
+++ part4/bloglist/backend/tests/list_helper.test.js
@@ -1,10 +1,40 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const list_of_blogs = require('./test_input').list_of_blogs
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('equal zero when list is empty', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has several blogs equals the likes the sum', () => {
+    const result = listHelper.totalLikes(list_of_blogs)
+    assert.strictEqual(result, 36)
+  })
+
 })
