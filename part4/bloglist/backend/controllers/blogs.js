2ddd2697ea913384c6cc507b4d6c8074a6b2f099
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-08-21 20:41:49 +0300

    Add exercise 4.13

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { response } = require('../app')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -24,6 +25,11 @@ blogsRouter.post('/', async (request, response) => {
   else {
     response.status(400).end()
   }
+})
+
+blogsRouter.delete('/:id', async(request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
--- part4/bloglist/backend/tests/blog_api.test.js
+++ part4/bloglist/backend/tests/blog_api.test.js
@@ -109,7 +109,26 @@ describe("blog api", () => {
     
 })
 
+describe('deletion of a blog', () => {
+    test('succeeds with status 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      assert.strictEqual(blogsAtEnd.length, helper.listOfBlogs.length - 1)
+
+      const ids = blogsAtEnd.map(r => r.id)
+      assert(!ids.includes(blogToDelete.id))
+    })
+  })
+
 })
+
 
 after(async () => {
     await mongoose.connection.close()
