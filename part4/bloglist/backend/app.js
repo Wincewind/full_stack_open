874891273d5e01f09a16af13ffeb30cb0943911d
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-08-23 18:19:06 +0300

    Add exercise 4.15

--- part4/bloglist/backend/app.js
+++ part4/bloglist/backend/app.js
@@ -3,10 +3,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-// mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -21,5 +22,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
