7b2ee67f4eed27d704a2672ad58ca9050bffdebc
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-08-24 14:39:53 +0300

    Add exercise 4.22

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -1,25 +1,16 @@
-const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
+const middleware = require('../utils/middleware')
 
-const decodeTokenAndFindUser = async (token, response) => {
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  return await User.findById(decodedToken.id)
-}
-
-blogsRouter.get('/', async (request, response) => {
+blogsRouter.get('/', async(request, response) => {
   const blogs = await Blog
   .find({}).populate('user', {username: 1, name: 1})
   response.status(200).json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async(request, response) => {
   const body = request.body
-  const user = await decodeTokenAndFindUser(request.token, response)
+  const user = request.user
 
   const blog = new Blog({
     title: body.title,
@@ -36,8 +27,8 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async(request, response) => {
-  const user = await decodeTokenAndFindUser(request.token, response)
+blogsRouter.delete('/:id', middleware.userExtractor, async(request, response) => {
+  const user = request.user
 
   const blog = await Blog.findById(request.params.id)
   if (blog === null) {
