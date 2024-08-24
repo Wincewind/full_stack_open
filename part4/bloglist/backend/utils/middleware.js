eaad50ec01ce19b5cb2982f44b801b2184608810
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-08-24 12:11:16 +0300

    Add exercise 4.20

--- part4/bloglist/backend/utils/middleware.js
+++ part4/bloglist/backend/utils/middleware.js
@@ -26,8 +26,17 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  request.token = (authorization && authorization.startsWith('Bearer '))
+  ? authorization.replace('Bearer ', '')
+  : null
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
