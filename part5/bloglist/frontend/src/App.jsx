92e1e1a48638a5f6fe793ee23cedc5ad5d10456d
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-08-28 10:40:26 +0300

    Add exercise 5.3

--- part5/bloglist/frontend/src/App.jsx
+++ part5/bloglist/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
+import { Blog, BlogForm } from './components/Blog'
 import LoginForm from './components/Login'
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -9,6 +9,7 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [newBlog, setNewBlog] = useState({ title: '', author: '', url: '' })
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -21,6 +22,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -35,6 +37,7 @@ const App = () => {
         'loggedBlogAppUser', JSON.stringify(user)
       )
       setUser(user)
+      blogService.setToken(user.token)
       setUsername('')
       setPassword('')
     } catch (exception) {
@@ -46,6 +49,15 @@ const handleLogout = async (event) => {
   event.preventDefault()
   delete window.localStorage['loggedBlogAppUser']
   setUser(null)
+  blogService.setToken('')
+}
+
+const addBlog = async (event) => {
+  event.preventDefault()
+
+  const addedBlog = await blogService.create(newBlog)
+  setBlogs(blogs.concat(addedBlog))
+  setNewBlog({ title: '', author: '', url: '' })
 }
 
   if (user === null) {
@@ -63,6 +75,7 @@ const handleLogout = async (event) => {
       <div>{user.name} logged in
         <button onClick={handleLogout} >logout</button>
       </div>
+      <BlogForm addBlog={addBlog} newBlog={newBlog} setNewBlog={setNewBlog}/>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- part5/bloglist/frontend/src/components/Blog.jsx
+++ part5/bloglist/frontend/src/components/Blog.jsx
@@ -4,4 +4,40 @@ const Blog = ({ blog }) => (
   </div>  
 )
 
-export default Blog
+const BlogForm = ({addBlog, newBlog, setNewBlog}) => (
+  <div>
+    <h2>create new</h2>
+    <form onSubmit={addBlog}>
+      <div>
+        title:
+          <input
+          type="text"
+          value={newBlog.title}
+          name="title"
+          onChange={({ target }) => setNewBlog((prevBlog) => ({...prevBlog, [target.name]: target.value}))}
+        />
+      </div>
+      <div>
+        author:
+          <input
+          type="text"
+          value={newBlog.author}
+          name="author"
+          onChange={({ target }) => setNewBlog((prevBlog) => ({...prevBlog, [target.name]: target.value}))}
+        />
+      </div>
+      <div>
+        url:
+          <input
+          type="text"
+          value={newBlog.url}
+          name="url"
+          onChange={({ target }) => setNewBlog((prevBlog) => ({...prevBlog, [target.name]: target.value}))}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  </div>
+)
+
+export { Blog, BlogForm }
